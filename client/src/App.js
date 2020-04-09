2f05fca3dfab17dac32a90d7ece7501b7b9cfe0c
viraj2896 <virajsheth84@gmail.com>
2020-04-09 00:02:48 -0400

    front/backend register,forget,about,contact

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,12 @@ import Register from './components/register/register';
 import RSVP from "./components/rsvp/Rsvp";
 import EventInvitation from './components/event-invitation/event-invitation';
 import EditProfile from './components/edit-profile/EditProfile';
+import Forgetpassword from './components/login/forgetpassword';
+import Resetpassword from './components/login/resetpassword';
+
+import EventList from './components/eventlist/eventlist';
+import ContactUs from './components/contactus';
+import Aboutus from './components/aboutus';
 
 function App() {
   return (
@@ -18,6 +24,11 @@ function App() {
           <Route path='/rsvp' component={RSVP} />
           <Route path='/invite' component={EventInvitation} />
           <Route path='/edit-profile' component={EditProfile} />
+          <Route path='/forgetpassword' component={Forgetpassword} />
+          <Route path='/resetpassword' component={Resetpassword} />
+          <Route path='/eventlist' component={EventList} />
+          <Route path='/contactus' component={ContactUs} />
+          <Route path='/aboutus' component={Aboutus} />
         </Switch>
       </div>
     </Router>
