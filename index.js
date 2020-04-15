557cd38b53350ae6a2180825c9e6b4bd7bd62684
Harshil Patel <patelharshil737@gmail.com>
2020-04-14 23:38:45 -0400

    path updated

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ connectDB();
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('*', function(req, res) {
+app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 
